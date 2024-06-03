8219a7f388ef42f4312c95394929ea4bedd85af8
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-06-03 21:44:14 +0100

    added a plus button which will be used to add exercises

--- script.js
+++ script.js
@@ -4,20 +4,18 @@
 //global variables 
 
 //record button 
-const recordButton = document.getElementById("recordButton");
+const addButton = document.getElementById("exercise");
 
 
 // when the recordWorkout button is clicked, the recordWorkout function is called
 function recordWorkout(){
-    recordButton.style.display = "none";
+    addButton.style.display = "none";
     //create exercise to add 
     const workout = document.createElement("div");
     workout.classList.add("workout");
 
     //add exercise button
-    const addExerciseButton = document.createElement("button");
-    addExerciseButton.classList.add("addExercise");
-    addExerciseButton.innerHTML = "Add Exercise";
+    
     
 
 
